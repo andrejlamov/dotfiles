0da71d97ef6a66c5d120f37e1a855ba88b46101a
Andrej Lamov <andrej.lamov@gmail.com>
2018-07-18 00:17:38 +0100

    Remove comments and auto-generated custom-sets

--- init.el
+++ init.el
@@ -26,23 +26,6 @@
 (setq straight-use-package-by-default t)
 (straight-use-package 'use-package)
 
-;;(require 'package)
-;;
-;;(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-;;                    (not (gnutls-available-p))))
-;;       (proto (if no-ssl "http" "https")))
-;;  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-;;  (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t))
-;;
-;;(package-initialize)
-;;
-;;(unless (package-installed-p 'use-package)
-;;  (package-refresh-contents)
-;;  (package-install 'use-package))
-;;(eval-when-compile
-;;  (require 'use-package))
-;;(setq use-package-always-ensure t)
-
 (defun almacs/core-configure ()
   (show-paren-mode)
   (winner-mode)
@@ -99,18 +82,4 @@
 		   gc-cons-threshold 800000
 		   gc-cons-percentage 0.1)
 	     (garbage-collect)))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (helm-git-grep helm-ls-git evil-magit cider switch-buffer-functions which-key clojure-mode evil-cleverparens smartparens helm-swoop paredit evil-collection company avy iedit helm general auto-package-update evil use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; end
