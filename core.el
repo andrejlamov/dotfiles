0031db9112f00f9c137b7eee150a38cb0d38130d
Andrej Lamov <andrej.lamov@gmail.com>
2018-07-20 14:49:29 +0200

    Disable annoying space when inserting

--- core.el
+++ core.el
@@ -79,6 +79,7 @@
   (add-hook 'clojure-mode-hook #'evil-cleverparens-mode)
   (add-hook 'lisp-mode-hook #'evil-cleverparens-mode)
   (add-hook 'emacs-lisp-mode-hook #'evil-cleverparens-mode)
+  (setq evil-cleverparens-use-regular-insert t)
   (smartparens-global-strict-mode))
 
 (use-package clojure-mode)
