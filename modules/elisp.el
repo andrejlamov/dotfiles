fb07972d3d787abb88170e2795aa522d8d05a92d
Andrej Lamov <andrej.lamov@gmail.com>
2018-09-27 21:57:21 +0200

    Test with ert

--- modules/elisp.el
+++ modules/elisp.el
@@ -1,8 +1,15 @@
+(defun almacs/elisp-test-buffer ()
+  (interactive)
+  (eval-buffer)
+  (ert t))
+
 (predd-defmethod almacs/major-mode-change 'emacs-lisp-mode (mode)
   (general-define-key
    :keymaps '(normal)
    :prefix ","
+   "i" '(indent-sexp :wk "indent sexp")
    "e" '(:ignore t :wk "eval")
    "eb" '(eval-buffer :wk "buffer")
-   "ee" '(eval-last-sexp :wk "sexp")))
-
+   "ee" '(eval-last-sexp :wk "sexp")
+   "t" '(:ignore t :wk "test")
+   "tB" '(almacs/elisp-test-buffer :wk "test buffer")))
