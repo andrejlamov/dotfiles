eef1be3239f5078e86d8edc1458d21572894fa1e
Andrej Lamov <andrej.lamov@gmail.com>
2018-09-30 12:59:03 +0200

    Add ert t

--- modules/elisp.el
+++ modules/elisp.el
@@ -3,6 +3,10 @@
   (eval-buffer)
   (ert t))
 
+(defun almacs/elisp-ert-t ()
+  (interactive)
+  (ert t))
+
 (predd-defmethod almacs/major-mode-change 'emacs-lisp-mode (mode)
   (general-define-key
    :keymaps '(normal)
@@ -12,4 +16,5 @@
    "eb" '(eval-buffer :wk "buffer")
    "ee" '(eval-last-sexp :wk "sexp")
    "t" '(:ignore t :wk "test")
+   "tt" '(almacs/elisp-ert-t :wk "test buffer")
    "tB" '(almacs/elisp-test-buffer :wk "test buffer")))
