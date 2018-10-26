04c97049d9ddf6005f638b17d6ca2e177e9c3e4c
Andrej Lamov <andrej.lamov@gmail.com>
2018-10-27 01:33:22 +0200

    More stupid theme

--- theme.el
+++ theme.el
@@ -1,7 +1,7 @@
 (custom-set-faces
  '(helm-ls-git-modified-not-staged-face ((t (:foreground "dark orange"))))
- '(mode-line-inactive ((t (:foreground "#002b36" :background "#fdf6e3" :box (:line-width 1 :style released-button)))))
- '(mode-line ((t (:background "#002b36" :foreground "#fdf6e3" :box (:line-width 1 :style pressed-button))))))
+ '(mode-line-inactive ((t (:foreground "#073642" :background "#eee8d5" :box (:line-width -1 :style released-button)))))
+ '(mode-line ((t (:background "#073642" :foreground "#fdf6e3" :box (:line-width -1 :style pressed-button))))))
 
 (set-background-color "#fdf6e3")
 (set-foreground-color "#002b36")
