41d33ff389c705eca08b323118650d6d45c8528f
Andrej Lamov <andrej.lamov@gmail.com>
2018-10-27 00:23:46 +0200

    Worst theme ever

--- theme.el
+++ theme.el
@@ -1,5 +1,8 @@
 (custom-set-faces
- '(helm-ls-git-modified-not-staged-face ((t (:foreground "dark orange")))))
+ '(helm-ls-git-modified-not-staged-face ((t (:foreground "dark orange"))))
+ '(mode-line-inactive ((t (:foreground "#002b36" :background "#fdf6e3" :box (:line-width 1 :style released-button)))))
+ '(mode-line ((t (:background "#002b36" :foreground "#fdf6e3" :box (:line-width 1 :style pressed-button))))))
+
 (set-background-color "#fdf6e3")
 (set-foreground-color "#002b36")
 (set-frame-font "Source code pro")
