7884bba1f082f5696f10571fd6dee8ec783cd519
Andrej Lamov <andrej.lamov@gmail.com>
2018-11-18 14:47:00 +0100

    Simplify emacs theme - look the same in X and no-window

--- emacs.d/theme.el
+++ emacs.d/theme.el
@@ -1,10 +1,6 @@
-(set-background-color "#fdf6e3")
-(set-foreground-color "#002b36")
-(set-frame-font "Source code pro")
-
-(custom-set-faces
- '(helm-ls-git-modified-not-staged-face ((t (:foreground "dark orange"))))
- '(mode-line-inactive ((t (:foreground "#073642" :background "#eee8d5"))))
- '(mode-line ((t (:background "#073642" :foreground "#fdf6e3"  )))))
-
-
+(when (display-graphic-p)
+  (set-foreground-color "#002b36")
+  (set-background-color "#fdf6e3")
+  (set-frame-font "Source code pro")
+  (custom-set-faces
+   '(helm-ls-git-modified-not-staged-face ((t (:foreground "dark orange"))))))
