4374d6feedcfc94028c7af96fa3674261f43134d
Andrej Lamov <andrej@machine.localdomain>
2018-12-31 01:51:57 +0100

    Add ",f" for quick defnformat

--- emacs.d/modules/clojure.el
+++ emacs.d/modules/clojure.el
@@ -60,4 +60,5 @@
  "ee" '(cider-pprint-eval-last-sexp :wk "eval sexp")
  "ef" '(cider-pprint-eval-defun-at-point :wk "eval defun")
  "bc" '(cider-repl-clear-buffer :wk "cider repl clear")
+ "f" '(cider-format-defun :wk "format defun")
  "1" '(almacs/clj-repl-purpose :wk "clj repl purpose"))
