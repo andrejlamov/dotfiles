c9a30c72cf54a4c6faabe53a9226459b51ec186b
Andrej Lamov <andrej@machine.localdomain>
2019-02-17 01:19:24 +0100

    Again the switch buffer keys

--- emacs.d/keys.el
+++ emacs.d/keys.el
@@ -47,8 +47,8 @@
   "S" '(helm-swoop :wk "swoop input")
   "SPC" '(helm-M-x :wk "M-x")
 
-  "<backtab>" '(evil-buffer :wk "toggle buffer")
-  "TAB" '(bs-cycle-previous :wk "prev buffer")
+  "TAB" '(evil-buffer :wk "toggle buffer")
+  "<backtab>" '(bs-cycle-previous :wk "prev buffer")
   "C-M-i" '(bs-cycle-next :wk "next buffer")
 
   "a" '(:ignore t :wk "stuff")
