9c1c88d74d7cbba1af548922121c2ad435c790c1
Andrej Lamov <andrej@machine.localdomain>
2019-03-25 22:19:22 +0100

    Quick buffer switching

--- emacs.d/funs.el
+++ emacs.d/funs.el
@@ -64,4 +64,53 @@
 
 (defun almacs/bash-term ()
   (interactive)
-  (ansi-term "/bin/bash"))
+  (ansi-term "/bin/bash")
+  (rename-buffer "term" t))
+
+(defun almacs/x-screensaver ()
+  (interactive)
+  (async-shell-command "xscreensaver-command -lock" nil nil))
+
+(defun almacs/non-asterisk-other-hidden-buffers ()
+  (let ((other-visible-buffers (->> (window-list)
+                                   (-map (-compose 'buffer-name 'window-buffer))
+                                   (-remove-item (buffer-name)))))
+    (->> (buffer-list)
+         (-map 'buffer-name)
+         (-filter (lambda (b) (not (-contains? other-visible-buffers b))))
+         (-filter (lambda (b) (not (s-matches? "\*.*\*" (s-trim b)))))
+         (-sort (lambda (a b) (s-less? (s-lower-camel-case a)
+                                       (s-lower-camel-case b)))))))
+
+(defun almacs/-switch-buffer (next-idx-fn)
+  (let* ((buffers (almacs/non-asterisk-other-hidden-buffers))
+         (max-idx (1- (length buffers)))
+         (current-idx (-elem-index (buffer-name) buffers)))
+    (if current-idx
+        (let ((next-idx (funcall next-idx-fn current-idx max-idx)))
+          (switch-to-buffer (nth next-idx buffers) t t))
+      (switch-to-buffer (car buffers) t t))))
+
+(defun almacs/prev-buffer ()
+  (interactive)
+  (almacs/-switch-buffer (lambda (current-idx max-idx)
+                          (cond
+                           ((eq current-idx 0) max-idx)
+                           (t (1- current-idx))))))
+
+
+(defun almacs/next-buffer ()
+  (interactive)
+  (almacs/-switch-buffer (lambda (current-idx max-id)
+                           (cond
+                            ((eq current-idx max-idx) 0)
+                            (t (1+ current-idx))))))
+
+(defun almacs/switch-buffer-by-first-char (char)
+  (interactive "c")
+  (when-let ((b (->> (buffer-list)
+                     (-map 'buffer-name)
+                     (-filter (lambda (b) (not (s-equals? (buffer-name) (s-trim b)))))
+                     (-sort 's-less?)
+                     (-find (lambda (b) (s-starts-with? (char-to-string char) (s-trim b) t))))))
+    (switch-to-buffer b t t)))
