618184339a8f698e8618190b7afd1b6c38eecd91
Andrej Lamov <andrej.lamov@gmail.com>
2019-04-08 21:40:49 +0200

    Nicer modeline colors

--- emacs.d/theme.el
+++ emacs.d/theme.el
@@ -2,5 +2,7 @@
   (set-foreground-color "#002b36")
   (set-background-color "#fdf6e3")
   (set-frame-font "Source code pro")
-  (custom-set-faces)
-   '(helm-ls-git-modified-not-staged-face ((t (:foreground "dark orange")))))
+  (custom-set-faces
+   '(mode-line ((t (:background "#002b36" :foreground "#fdf6e3"))))
+   '(mode-line-inactive ((t (:background "#eee8d5"))))
+   '(helm-ls-git-modified-not-staged-face ((t (:foreground "dark orange"))))))
