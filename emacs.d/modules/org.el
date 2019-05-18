13fdd6c1471a0fcc8a164717a1d87dc0737aa2e8
Andrej Lamov <andrej.lamov@gmail.com>
2019-05-18 16:17:05 +0200

    Add org-mind-map package

--- emacs.d/modules/org.el
+++ emacs.d/modules/org.el
@@ -1,9 +1,13 @@
+(use-package org-mind-map
+  :init (require 'ox-org)
+  :config (setq org-mind-map-engine "dot"))
+
 (use-package evil-org
   :after org
   :config
 
   (org-babel-do-load-languages 'org-babel-load-languages
-    '((shell . t)))
+                               '((shell . t)))
 
   (defun almacs/org/confirm-babel-evaluate (lang body) nil)
   (setq org-confirm-babel-evaluate 'almacs/org/confirm-babel-evaluate)
@@ -17,3 +21,15 @@
   (evil-define-key 'normal org-mode-map
     (kbd "TAB") 'org-cycle
     "t" 'org-todo))
+
+(general-create-definer org-def
+  :states '(normal)
+  :keymaps '(org-mode-map evil-org-mode-map)
+  :prefix ","
+  :keymaps 'override)
+
+(org-def
+  "m" '(:ignore t :wk "mind map")
+  "mf" '(org-mind-map-write :wk "file")
+  "mt" '(org-mind-map-write-current-tree :wk "tree")
+  "mb" '(org-mind-map-write-current-branch :wk "branch"))
