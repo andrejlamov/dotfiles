6a363bce66f223014763e2dab6154ed4e3148f6a
Andrej Lamov <andrej@machine.localdomain>
2019-09-04 01:55:50 +0200

    asdf

--- emacs.d/theme.el
+++ emacs.d/theme.el
@@ -16,10 +16,54 @@
                         :italic nil)))
 
 (require 'color)
-(-map (lambda (face)
-        (let ((color (face-attribute face :foreground)))
-          (when (not (eq color 'unspecified))
-            (let* ((rgb (color-values color))
-                   (hsl (apply 'color-rgb-to-hsl rgb)))
-              (list color hsl)))))
-      (face-list))
+
+(defun h-to-domain (domain h)
+  (let ((range (/ 1.0 (length domain))))
+    (->> domain
+         (-map-indexed
+          (lambda (idx d)
+            (list
+             (* (1+ idx) range) d)))
+         (-first (-lambda ((r d)) (< h r)))
+         (-second-item))))
+
+'(-each (face-list)
+   (lambda (face)
+     (let ((color (face-attribute face :foreground)))
+       (when (not (eq color 'unspecified))
+         (-let* ((rgb (color-values color))
+                 ((h s l) (apply 'color-rgb-to-hsl rgb))
+                 (c (h-to-domain
+                     '("RoyalBlue1" "RoyalBlue1"
+                       "RoyalBlue1" "RoyalBlue1"
+                       "RoyalBlue2" "RoyalBlue2"
+                       "RoyalBlue2" "RoyalBlue2"
+                       "RoyalBlue3" "RoyalBlue3"
+                       "RoyalBlue3" "RoyalBlue3"
+                       "RoyalBlue4" "RoyalBlue4"
+                       "RoyalBlue4" "RoyalBlue4"
+                       "SlateBlue1" "SlateBlue1"
+                       "SlateBlue1" "SlateBlue1"
+                       "SlateBlue2" "SlateBlue2"
+                       "SlateBlue2" "SlateBlue2"
+                       "SlateBlue3" "SlateBlue3"
+                       "SlateBlue3" "SlateBlue3"
+                       "SlateBlue4" "SlateBlue4"
+                       "SlateBlue4" "SlateBlue4"
+                       ) h)))
+           (set-face-attribute face nil
+                               :foreground c))))))
+
+(->>
+ (face-list)
+ (-map (lambda (face)
+         (let ((color (face-attribute face :foreground)))
+           (when (not (eq color 'unspecified))
+             (-let* ((rgb (color-values color))
+                     ((h s l) (apply 'color-rgb-to-hsl rgb))
+                     (c (h-to-domain
+                         '("brown1" "brown2" "brown3" "brown4") h)))
+               (set-face-attribute face nil
+                                   :foreground c)
+               h)))))
+ (-filter 'numberp))
