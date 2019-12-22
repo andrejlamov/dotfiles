053c0474cb52771b031299d98fb2e7019f97160f
Andrej Lamov <andrej.lamov@gmail.com>
2019-12-22 22:19:22 +0100

    Redefine sp-forward-sexp to move from end of line in normal mode

--- emacs.d/base-packages.el
+++ emacs.d/base-packages.el
@@ -67,7 +67,28 @@
   :config
   ;; https://github.com/Fuco1/smartparens/issues/908
   (sp-local-pair sp-lisp-modes  "'" 'nil :actions 'nil)
-  (sp-local-pair sp-lisp-modes  "`" 'nil :actions 'nil))
+  (sp-local-pair sp-lisp-modes  "`" 'nil :actions 'nil)
+
+  ;; Redefine sp-forward-sexp to fix end of line in evil
+  (defun sp-forward-sexp (&optional arg)
+    (interactive "^p")
+    (setq arg (or arg 1))
+    (if (< arg 0)
+        (sp-backward-sexp (- arg))
+      (let* ((n arg)
+             (ok t))
+        (while (and ok (> n 0))
+          (when (and
+                 (> arg 0)
+                 (eq (point)
+                     (save-excursion
+                       (call-interactively 'evil-end-of-line)
+                       (point))))
+            (goto-char (1+ (point))))
+          (setq ok (sp-get-thing))
+          (setq n (1- n))
+          (when ok (goto-char (sp-get ok :end))))
+        ok))))
 
 (use-package paredit)
 
