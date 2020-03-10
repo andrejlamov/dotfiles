744f70118730ee28768302de48b50769601ab3c9
Andrej Lamov <andrej.lamov@gmail.com>
2020-03-10 23:48:09 +0100

    Tweak wgrep

--- emacs.d/modules/wgrep.el
+++ emacs.d/modules/wgrep.el
@@ -1,9 +1,19 @@
 (use-package wgrep)
+
 (use-package wgrep-helm
   :config
-  (add-hook 'helm-grep-mode-hook 'wgrep-change-to-wgrep-mode)
-  (add-hook 'helm-git-grep-mode-hook 'wgrep-change-to-wgrep-mode)
-  (add-hook 'helm-occur-mode-hook 'wgrep-change-to-wgrep-mode)
+
+  (setq wgrep-auto-save-buffer t
+        wgrep-change-readonly-file t)
+
+  (defun almacs/force-wgrep ()
+    (grep-mode)
+    (wgrep-change-to-wgrep-mode))
+
+  (add-hook 'grep-mode-hook 'almacs/force-wgrep)
+  (add-hook 'helm-grep-mode-hook 'almacs/force-wgrep)
+  (add-hook 'helm-git-grep-mode-hook 'almacs/force-wgrep)
+  (add-hook 'helm-occur-mode-hook 'almacs/force-wgrep)
 
   (general-create-definer wgrep-def
     :states '(normal)
@@ -14,13 +24,10 @@
   (wgrep-def
     "w" '(wgrep-change-to-wgrep-mode :wk "wgrep"))
 
-  (defun almacs/wgrep-jump-to ()
-    (interactive)
-    (pcase major-mode
-      ('helm-grep-mode (helm-grep-mode-jump-other-window))
-      ('helm-occur-mode (helm-occur-mode-goto-line))
-      (other (message "other"))))
-
+  (evil-collection-define-key 'normal 'helm-grep-mode-map
+    (kbd "C-l") 'compile-goto-error)
+  (evil-collection-define-key 'normal 'grep-mode-map
+    (kbd "C-l") 'compile-goto-error)
   (evil-collection-define-key 'normal 'wgrep-mode-map
-    (kbd "C-l") #'almacs/wgrep-jump-to))
+    (kbd "C-l") 'compile-goto-error))
 
