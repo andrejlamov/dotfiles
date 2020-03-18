b0b78af1e1452c5cddd54d3442d71536f891ed72
Andrej Lamov <andrej.lamov@gmail.com>
2020-03-18 09:38:32 +0100

    Add keys for setting priority in org

--- emacs.d/modules/org.el
+++ emacs.d/modules/org.el
@@ -52,6 +52,11 @@
   "ls" '(org-store-link :wk "store")
   "li" '(org-insert-link :wk "insert")
 
+  "p" '(:ignore t :wk "prio")
+  "pp" '(org-priority :wk "up")
+  "pk" '(org-priority-up :wk "up")
+  "pj" '(org-priority-down :wk "down")
+
   "m" '(:ignore t :wk "mind map")
   "mf" '(org-mind-map-write :wk "file")
   "mt" '(org-mind-map-write-current-tree :wk "tree")
