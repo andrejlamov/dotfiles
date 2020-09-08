4aa4daa5cd54a373e82b5965da8ea800344c5b1f
Andrej Lamov <andrej.lamov@gmail.com>
2020-09-08 09:16:07 +0200

    Theme update

--- emacs.d/theme.el
+++ emacs.d/theme.el
@@ -10,4 +10,6 @@
        '(mode-line-inactive ((t (:background "brightwhite"))))
        '(helm-ls-git-modified-not-staged-face ((t (:foreground "dark orange"))))))
   (custom-set-faces
+   '(mode-line ((t (:background "black" :foreground "brightwhite"))))
+   '(whitespace-tab ((t (:background "grey90"))))
    '(region ((t (:background "#eee8d5"))))))
