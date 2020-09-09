c4634fd7e08e426ef128f5746d1ead815403a09c
Andrej Lamov <andrej.lamov@gmail.com>
2020-09-09 17:23:17 +0200

    Ditch X emacs

--- emacs.d/theme.el
+++ emacs.d/theme.el
@@ -1,15 +1,4 @@
-(if (display-graphic-p)
-    (progn
-      (set-foreground-color "#002b36")
-      (set-background-color "#fdf6e3")
-      (set-frame-font "Source code pro")
-      (custom-set-faces
-       '(magit-diff-context-highlight ((t (:background "#fdf6e3"))))
-       '(region ((t (:background "#eee8d5"))))
-       '(mode-line ((t (:background "brightblack" :foreground "brightwhite"))))
-       '(mode-line-inactive ((t (:background "brightwhite"))))
-       '(helm-ls-git-modified-not-staged-face ((t (:foreground "dark orange"))))))
-  (custom-set-faces
-   '(mode-line ((t (:background "black" :foreground "brightwhite"))))
-   '(whitespace-tab ((t (:background "grey90"))))
-   '(region ((t (:background "#eee8d5"))))))
+(custom-set-faces
+ '(mode-line ((t (:background "black" :foreground "white"))))
+ '(whitespace-tab ((t (:background "grey90"))))
+ '(region ((t (:background "#eee8d5")))))
