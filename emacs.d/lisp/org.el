e4e0395e8fdcf974c0f15b147ae47fd56940530d
Andrej Lamov <andrej.lamov@gmail.com>
2020-09-19 14:40:14 +0200

    Use evil-org (again!?)

--- emacs.d/lisp/org.el
+++ emacs.d/lisp/org.el
@@ -1,41 +1,56 @@
-(use-package org-evil
-  :config
-  (require 'org-evil)
-  (require' org-id)
-  (setq
-   org-id-link-to-org-use-id t
-   org-directory "~/Dropbox/org"
-   org-default-notes-file (concat org-directory "/inbox.org")
-   org-capture-templates
-   '(("p" "Capture at point" entry (file "~/Dropbox/org/inbox.org")
-      "* %?\n  %i\n  %a")
-     ("c" "Capture" entry (file "~/Dropbox/org/inbox.org")
-      "* %?"))
-   org-agenda-start-day "-1d"
-   org-startup-indented t
-   org-agenda-span 14
-   org-agenda-start-on-weekday nil
-   org-startup-with-inline-images t
-   org-refile-use-outline-path nil
-   org-agenda-files '("~/Dropbox/org/inbox.org"
-                      "~/Dropbox/org/projects.org"
-                      "~/Dropbox/org/reference.org"
-                      "~/Dropbox/org/someday.org"
-                      "~/Dropbox/org/calendar.org")
-   org-refile-targets '((("~/Dropbox/org/projects.org") :maxlevel . 1)))
-  ;; eval src blocks
-  (org-babel-do-load-languages 'org-babel-load-languages
-                               '((shell . t)))
-  (org-display-inline-images t t)
-  (defun almacs/org/confirm-babel-evaluate (lang body) nil)
-  (setq org-confirm-babel-evaluate 'almacs/org/confirm-babel-evaluate)
+;; (use-package org-evil
+;;   :config
+;;   (require 'org-evil)
+;;   (require' org-id)
+;;   (setq
+;;    org-id-link-to-org-use-id t
+;;    org-directory "~/Dropbox/org"
+;;    org-default-notes-file (concat org-directory "/inbox.org")
+;;    org-capture-templates
+;;    '(("p" "Capture at point" entry (file "~/Dropbox/org/inbox.org")
+;;       "* %?\n  %i\n  %a")
+;;      ("c" "Capture" entry (file "~/Dropbox/org/inbox.org")
+;;       "* %?"))
+;;    org-agenda-start-day "-1d"
+;;    org-startup-indented t
+;;    org-agenda-span 14
+;;    org-agenda-start-on-weekday nil
+;;    org-startup-with-inline-images t
+;;    org-refile-use-outline-path nil
+;;    org-agenda-files '("~/Dropbox/org/inbox.org"
+;;                       "~/Dropbox/org/projects.org"
+;;                       "~/Dropbox/org/reference.org"
+;;                       "~/Dropbox/org/someday.org"
+;;                       "~/Dropbox/org/calendar.org")
+;;    org-refile-targets '((("~/Dropbox/org/projects.org") :maxlevel . 1)))
+;;   ;; eval src blocks
+;;   (org-babel-do-load-languages 'org-babel-load-languages
+;;                                '((shell . t)))
+;;   (org-display-inline-images t t)
+;;   (defun almacs/org/confirm-babel-evaluate (lang body) nil)
+;;   (setq org-confirm-babel-evaluate 'almacs/org/confirm-babel-evaluate)
 
-  '(defun almacs/org-insert-custom-id (&rest rest)
-     (org-set-property "CUSTOM_ID" (s-concat "id-" (almacs/uuid))))
-  '(advice-add 'org-insert-heading-respect-content :after #'almacs/org-insert-custom-id)
-  (general-define-key
-   :keymaps 'org-mode-map
-   [remap evil-jump-forward] 'org-cycle))
+;;   '(defun almacs/org-insert-custom-id (&rest rest)
+;;      (org-set-property "CUSTOM_ID" (s-concat "id-" (almacs/uuid))))
+;;   '(advice-add 'org-insert-heading-respect-content :after #'almacs/org-insert-custom-id)
+;;   (general-define-key
+;;    :keymaps 'org-mode-map
+;;    [remap evil-jump-forward] 'org-cycle))
+
+(use-package evil-org
+  :ensure t
+  :after org
+  :config
+  (require 'org-id)
+  (setq org-id-link-to-org-use-id t)
+  (add-hook 'org-mode-hook 'evil-org-mode)
+  (add-hook 'evil-org-mode-hook
+            (lambda ()
+              (evil-org-set-key-theme)))
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys)
+  (general-define-key :keymaps 'org-mode-map
+                      [remap evil-jump-forward] 'org-cycle))
 
 (use-package org-mind-map
   :init (require 'ox-org)
@@ -54,7 +69,17 @@
   "s" '(org-insert-structure-template :wk "template")
   "t" '(org-todo :wk "todo")
   "'" '(org-set-tags-command :wk "tag")
-  "l" '(:ignore t :wk "link")
+
+  ;; filter
+  "f" '(:ignore t :wk "filter")
+  "ft" '(org-show-todo-tree :wk "todo tree")
+
+  ;; list items
+  "i" '(:ignore t :wk "list items")
+  "it" '(org-toggle-radio-button :wk "radio")
+
+  ;; links
+  "l" '(:ignore t :wk "links")
   "ls" '(org-store-link :wk "store")
   "li" '(org-insert-link :wk "insert")
 
