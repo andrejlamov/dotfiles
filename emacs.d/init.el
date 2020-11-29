8fe556088b871282388b2ee490af94d2cb76dc3b
Andrej Lamov <andrej.lamov@gmail.com>
2020-11-29 16:09:21 +0100

    Start web-mode and tide when in jsx file

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -463,10 +463,39 @@
                      ",ef" cider-pprint-eval-defun-at-point))
 
 (use-package web-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode)) ;; auto-enable for .js/.jsx files
+  (setq
+   web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'"))
+   web-mode-enable-auto-pairing t
+   web-mode-enable-auto-expanding t
+   web-mode-enable-auto-closing t
+   web-mode-markup-indent-offset 2
+   web-mode-css-indent-offset 2
+   web-mode-code-indent-offset 2
+   web-mode-indent-style  1
+   web-mode-enable-css-colorization t)
+
+  (flycheck-add-mode 'javascript-eslint 'web-mode)
+
+  ;; (defun setup-tide-mode ()
+  ;;   (interactive)
+  ;;   (tide-setup)
+  ;;   (flycheck-mode +1)
+  ;;   (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  ;;   (eldoc-mode +1)
+  ;;   (tide-hl-identifier-mode +1)
+  ;;   (company-mode +1))
+
+  ;; (add-hook 'web-mode-hook
+  ;;           (lambda ()
+  ;;             (when (string-equal "jsx" (file-name-extension buffer-file-name))
+  ;;               (setup-tide-mode))))
+
+  :hook ((js-mode . web-mode))
   :general
   (:keymaps
    'web-mode-map
-
    :states '(normal visual)
    ",/" 'web-mode-element-close
    "a" 'web-mode-element-content-select
@@ -488,47 +517,20 @@
    ",v" 'web-mode-element-vanish
    ",w" 'web-mode-element-wrap
    ",+" 'web-mode-element-extract
-   ",-" 'web-mode-element-contract)
-
-  :config
-  (flycheck-add-mode 'javascript-eslint 'web-mode)
-  (flycheck-add-next-checker 'javascript-eslint 'jsx-tide 'append)
-  (flycheck-add-mode 'javascript-eslint 'web-mode)
-  (flycheck-add-next-checker 'javascript-eslint 'jsx-tide 'append)
-
-  (defun setup-tide-mode ()
-    (interactive)
-    (tide-setup)
-    (flycheck-mode +1)
-    (setq flycheck-check-syntax-automatically '(save mode-enabled))
-    (eldoc-mode +1)
-    (tide-hl-identifier-mode +1)
-    (company-mode +1))
-
-  (setq web-mode-content-types-alist
-        '(("jsx" . "\\.js[x]?\\'")))
-  (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
-  (add-hook 'web-mode-hook
-            (lambda ()
-              (when (string-equal "jsx" (file-name-extension buffer-file-name))
-                (setup-tide-mode))))
-
-  (setq web-mode-enable-auto-pairing t
-        web-mode-enable-auto-expanding t
-        web-mode-enable-auto-closing t
-        web-mode-markup-indent-offset 2
-        web-mode-css-indent-offset 2
-        web-mode-code-indent-offset 2
-        web-mode-indent-style  1
-        web-mode-enable-css-colorization t))
+   ",-" 'web-mode-element-contract))
 
 (use-package tide
   :ensure t
   :config
   :after (typescript-mode company flycheck)
   :hook ((typescript-mode . tide-setup)
-         (typescript-mode . tide-hl-identifier-mode)))
+         (web-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode))
+  :config
+  (flycheck-add-next-checker 'javascript-eslint 'jsx-tide 'append)
+  (flycheck-add-mode 'javascript-eslint 'web-mode)
+  (flycheck-add-next-checker 'javascript-eslint 'jsx-tide 'append))
+
 
 (use-package prettier-js
   :config '(add-hook 'web-mode-hook 'prettier-js-mode))
