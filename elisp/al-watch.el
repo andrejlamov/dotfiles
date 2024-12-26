5c802cc6e4299f9ca1d8e95fbadc42de34c9bdc9
Andrej Lamov <andrej.lamov@gmail.com>
2024-12-26 10:52:10 +0100

    al-watch: Format value according to `al-watch-formatter`

--- elisp/al-watch.el
+++ elisp/al-watch.el
@@ -6,6 +6,20 @@
 (require 'dash)
 
 (defvar al-watch-buffer-prefix "*al-watch*")
+(defvar al-watch-formatter 'al-watch-default-formatter)
+
+(defun al-watch-default-formatter (val)
+  (let ((res-list nil))
+    (if (listp val)
+        (progn
+          (push "(" res-list )
+          (-each-indexed val (lambda (i e)
+                               (unless (eq i 0)
+                                 (push  "\n " res-list))
+                               (push  (prin1-to-string e) res-list)))
+          (push  ")" res-list))
+      (push  (prin1-to-string val) res-list))
+    (apply 'concat (reverse res-list))))
 
 (defun al-watch-watcher-fn (symbol newval operation where)
   (let ((b (get-buffer-create (concat al-watch-buffer-prefix " " (symbol-name symbol)))))
@@ -15,7 +29,7 @@
         (erase-buffer)
         (insert ";; active watcher\n")
         (insert "\n")
-        (insert (prin1-to-string newval))))))
+        (insert (funcall al-watch-formatter newval))))))
 
 (defun al-watch-add-fn (symbol)
   (interactive "S")
