4f80bc90a71bd3ed63fc483722c7f90fd45be043
Andrej Lamov <andrej.lamov@gmail.com>
2025-06-21 09:59:44 +0000

    Default term is xterm-256color with truecolor
    Still not sure if I need to compile a custom terminfo or not.

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,7 @@ set -euxo pipefail
 
 ROOT=$(realpath "$0" | xargs dirname)
 ln -sf "$ROOT"/.emacs ~/.emacs
+ln -sf "$ROOT"/.bashrc ~/.bashrc
+ln -sf "$ROOT"/.bash_profile ~/.bash_profile
 ln -sf "$ROOT"/.gitconfig ~/.gitconfig
 mkdir -p ~/elisp && ln -sf "$ROOT"/elisp/* ~/elisp
