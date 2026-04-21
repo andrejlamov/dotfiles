f3f9ee78bdc3dbf11d67f7ec24fb601a61a28dd5
Andrej Lamov <andrej.lamov@gmail.com>
2026-04-21 20:35:39 +0200

    Adapt fontsize to asus zenbook

--- emacs/init.el
+++ emacs/init.el
@@ -42,8 +42,8 @@
 
   ;; theme
   (load-theme 'modus-operandi-tinted t nil)
-  (if (equal (system-name) "void")
-      (set-face-attribute 'default nil :height 80)
+  (if (equal (system-name) "asus-void")
+      (set-face-attribute 'default nil :height 105)
     (set-face-attribute 'default nil :height 120))
 
   ;; (defun my/apply-monochrome-bold-syntax ()
